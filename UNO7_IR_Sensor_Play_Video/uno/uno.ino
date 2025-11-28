7d02a7739de81ce48d9ead9ddf2f5ec229665b33
Mohamed Ibrahim <119154176+mohamed-ibrahim-21@users.noreply.github.com>
2025-11-28 23:39:05 +0200

    Update uno.ino

--- UNO7_IR_Sensor_Play_Video/uno/uno.ino
+++ UNO7_IR_Sensor_Play_Video/uno/uno.ino
@@ -1,24 +1,23 @@
-const int sensorPin = 2;  // البن المتوصل بـ OUT
+const int sensorPin = 2; 
 int lastState = HIGH;
 
 void setup() {
   pinMode(sensorPin, INPUT);
-  Serial.begin(9600); // نفس البودريت اللي هنستخدمه في الكمبيوتر
+  Serial.begin(9600);
 }
 
 void loop() {
   int state = digitalRead(sensorPin);
 
-  // بعض الموديولات بتطلع LOW لما في حاجه قدامها
   if (state != lastState) {
     lastState = state;
 
-    if (state == LOW) {        // جرّب LOW الأول، ولو لقيته العكس خليه HIGH
+    if (state == LOW) {
       Serial.println("BOX_ON");
     } else {
       Serial.println("BOX_OFF");
     }
 
-    delay(50); // تهدئة بسيطة عشان ميقفلش ويفتح بسرعة
+    delay(50);
   }
 }
